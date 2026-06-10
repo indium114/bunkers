1366b696c172e0be9f1337d7078d6503267f8b78
indium114 <156162907+indium114@users.noreply.github.com>
2026-06-10 19:06:28 +0200

    feat: some clap command setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use clap::{Parser, Subcommand};
+
+static VERSION: &str = env!("CARGO_PKG_VERSION");
+
+#[derive(Parser)]
+#[command(name = "bunkers")]
+#[command(version = &VERSION)]
+#[command(about = "A CLI tool to manage LUKS-encrypted disk images", long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Create { name: String, size: u32 },
+    Mount { name: String },
+    Umount { name: String },
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Commands::Create { name, size } => {
+            println!("Called create with {} and {}", name, size);
+        }
+        Commands::Mount { name } => {
+            println!("Called mount with {}", name);
+        }
+        Commands::Umount { name } => {
+            println!("Called umount with {}", name);
+        }
+    }
 }
