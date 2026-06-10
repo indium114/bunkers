97412fd471f2c09936288d185555f2936c927a23
indium114 <156162907+indium114@users.noreply.github.com>
2026-06-10 21:13:08 +0200

    feat: loop_path safety check

--- src/create.rs
+++ src/create.rs
@@ -2,7 +2,7 @@ use crate::help;
 use std::process::{Command, Stdio};
 use which::which;
 
-pub fn create(name: &String, size: &u32) {
+pub fn create(name: &String, size: &u32) -> bool {
     // MARK: create the image
     let path: String = help::bunker_path(name.to_string());
 
@@ -18,14 +18,27 @@ pub fn create(name: &String, size: &u32) {
         Ok(_path) => "doas".to_string(),
         Err(_) => "sudo".to_string(),
     };
-    let device_path = Command::new(&elevator)
+    let loop_device = Command::new(&elevator)
         .arg("losetup")
         .arg("--find")
         .arg("--show")
         .arg(&path)
         .stdin(Stdio::inherit())
-        .stdout(Stdio::inherit())
         .stderr(Stdio::inherit())
         .output()
         .expect("failed to run losetup");
+
+    let loop_path: String = String::from_utf8(loop_device.stdout)
+        .unwrap()
+        .trim()
+        .to_string();
+
+    // MARK: make sure that device_path starts with /dev/loop
+    if !loop_path.starts_with("/dev/loop") {
+        println!("loop_path does not start with /dev/loop");
+        return false;
+    }
+    println!("loop_path starts with /dev/loop");
+
+    return true;
 }
