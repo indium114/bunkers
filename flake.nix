813a2731cbe2a8a3e2d7b6d4828e5a8aa0199610
indium114 <156162907+indium114@users.noreply.github.com>
2026-06-12 20:29:09 +0200

    chore: bump version

--- flake.nix
+++ flake.nix
@@ -29,7 +29,7 @@
 
         packages.bunkers = pkgs.rustPlatform.buildRustPackage {
           name = "bunkers";
-          version = "0.1.1";
+          version = "0.1.2";
 
           src = ./.;
 
